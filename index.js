8e14958092278241dab30fea891ea67d08550f31
Khadeejasherif <khadeejasherif372@gmail.com>
2026-07-20 02:58:39 +0300

    add update

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/cv', cvRouter);
 app.use('/services',serviceRoutes);
 app.use('/Skills',skillsRoutes);
 app.use('/contactUs',contactRouter);
-
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 
 //========error handling======/

b1b88cfe0885fd039372aa8c94d9f812d739f57e
Khadeejasherif <khadeejasherif372@gmail.com>
2026-07-20 17:02:50 +0300

    add

--- index.js
+++ index.js
@@ -80,7 +80,14 @@ app.use((req, res, next) => {
 //   .catch((err) => {
 //     console.log("cant connect to mongo",err);
 //   });
+app.listen(process.env.port,(err)=>{
+    if(err){
+        console.log("can't listen on the port");
+    }else{
+        console.log("listen on port successfully");
+    }
 
+});
 
 // 3. EXPORT FOR VERCEL (Remove app.listen)
 module.exports = app;
@@ -88,11 +95,3 @@ module.exports = app;
 
 
 
-// app.listen(process.env.port,(err)=>{
-//     if(err){
-//         console.log("can't listen on the port");
-//     }else{
-//         console.log("listen on port successfully");
-//     }
-
-// });
